c21118220cf69bbd070d5d850d7aacca02b0fdc6
Damian <sieradzki129@gmail.com>
2020-09-05 23:41:53 +0200

    v.0.8

--- checker.js
+++ checker.js
@@ -2,21 +2,24 @@ const MY_SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/T011VGC0VUJ/B01AJ
 const slack = require('slack-notify')(MY_SLACK_WEBHOOK_URL);
 const request = require('request');
 
+let  data = new Date();
+let seconds = data.getSeconds();
+let minutes = data.getMinutes();
+let hours  = data.getHours();
+
+let time = hours + ':' + minutes + ':' + seconds;
 setInterval(check, 3600000);
 
 function check () {
-request('https://lista-serwerow.pl/', function (error, response, body) {
+request('https://lista-serwerow.pl/', function (error, response) {
     if (!error && response.statusCode == 200) {
         console.log('Serwer działa poprawnie.')
         }
     else {
         slack.send({
             channel: '#lista-serwerów',
-            text: '*Serwer nie działa, sprawdź co poszło nie tak.*' + '\n' + '*Logi poniżej:*' + '\n' + JSON.stringify(response),
+            text: time + ' *Serwer nie działa, sprawdź co poszło nie tak.*' + '\n' + '*Logi poniżej:*' + '\n' + JSON.stringify(response),
             username: 'webChecker'
         })
     }}
 )}
-
-
-
